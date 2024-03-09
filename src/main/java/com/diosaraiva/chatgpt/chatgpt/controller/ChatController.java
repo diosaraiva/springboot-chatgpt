c356ba4d773cb7582e440e4069cdfa5713f20bf2
diosaraiva <diosaraiva@192.168.0.13>
2024-03-09 19:30:26 -0300

    refactor ok.

--- src/main/java/com/diosaraiva/chatgpt/chatgpt/controller/ChatController.java
+++ src/main/java/com/diosaraiva/chatgpt/chatgpt/controller/ChatController.java
@@ -2,7 +2,8 @@ package com.diosaraiva.chatgpt.chatgpt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,35 +13,36 @@ import org.springframework.web.client.RestTemplate;
 import com.diosaraiva.chatgpt.chatgpt.dto.ChatRequestDto;
 import com.diosaraiva.chatgpt.chatgpt.dto.ChatResponseDto;
 import com.diosaraiva.chatgpt.chatgpt.service.OpenAIService;
-import com.diosaraiva.chatgpt.utils.YamlPropertySourceFactory;
 
 @RestController
 @RequestMapping("/chatgpt")
-@PropertySource(value = "openai/openai.yaml", factory = YamlPropertySourceFactory.class)
 public class ChatController {
-    
-    @Autowired
-    private OpenAIService openAIService;
-    
-    @Value("${openai.model}")
-    private String model;
-    
-    @Value("${openai.api.url}")
-    private String apiUrl;
-    
-    @GetMapping("/chat")
-    public String chat(String openaiApiKey, @RequestParam String prompt) {
-        // create a request
-        ChatRequestDto request = new ChatRequestDto(model, prompt);
-        
-        RestTemplate restTemplate = openAIService.openaiRestTemplate(openaiApiKey);
-        ChatResponseDto response = restTemplate.postForObject(apiUrl, request, ChatResponseDto.class);
-        
-        if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
-            return "No response";
-        }
-        
-        // return the first response - TODO
-        return response.getChoices().get(0).getMessageDto().getContent();
-    }
+
+	@Autowired
+	private OpenAIService openAIService;
+
+	@Value("${openai.model}")
+	private String model;
+
+	@Value("${openai.api.url}")
+	private String apiUrl;
+
+	@GetMapping("/get")
+	public ResponseEntity<?> getChatGPTResponse(String openaiApiKey, @RequestParam String prompt) {
+
+		try {
+			ChatRequestDto request = new ChatRequestDto(model, prompt);
+			RestTemplate restTemplate = openAIService.openaiRestTemplate(openaiApiKey);
+			ChatResponseDto response = restTemplate.postForObject(apiUrl, request, ChatResponseDto.class);
+
+			if (response != null) {
+				//return response.getChoices().get(0).getMessageDto().getContent();
+				return new ResponseEntity<>(response, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.EXPECTATION_FAILED);
+		}		
+	}
 }
